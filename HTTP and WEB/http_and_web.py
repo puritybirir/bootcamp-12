bec9f9a9b71215d8124ab691db973d4d9b315e7d
Purity Birir <puritybirir@gmail.com>
2016-11-14 13:05:24 +0300

    Change the api to weather api.

--- HTTP and WEB/http_and_web.py
+++ HTTP and WEB/http_and_web.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python
 
 import requests
-def get_makeup():
-	"""Gets a list of makeup products and their various descriptions."""
-
-    #send a get request
-	response = requests.get("http://makeup-api.herokuapp.com/api/v1/products.json?brand=covergirl&product_type=lipstick")
+from pprint import pprint
+def main():
+	'''A weather application that shows the weather of a city and its temparature '''
+	city= "Nairobi"
+	response = requests.get("http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=31cb387d5a46c10fe1e0400a04d0721f&units=metric")
 	assert response.status_code == 200
+
+	weather=response.json()
+	print("The weather for",weather['name'])
+	print(weather['main']['temp'])
+	print(weather['weather'][0]['description'])
 
 	if response.status_code != 200:
 		raise Exception("API error. Response status: {}".format(response.status_code))
 	else:
 		return response.json
+
+if __name__ == '__main__':
+	main()
+
+	
