67a20e95dcc89ec8d87ca52ba19fe45ad520e07c
Purity Birir <puritybirir@gmail.com>
2016-11-14 12:28:15 +0300

    Add response status code

--- HTTP and WEB/http_and_web.py
+++ HTTP and WEB/http_and_web.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
 import requests
-from pprint import pprint
 def get_makeup():
 	"""Gets a list of makeup products and their various descriptions."""
 
     #send a get request
 	response = requests.get("http://makeup-api.herokuapp.com/api/v1/products.json?brand=covergirl&product_type=lipstick")
+	assert response.status_code == 200
 
 	if response.status_code != 200:
 		raise Exception("API error. Response status: {}".format(response.status_code))
